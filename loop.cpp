ad9b1b5407a21f0e7b2edf64da7b61b7c9e7a870
Danish Abdullah <danishabdullah276@gmail.com>
2023-09-24 00:59:44 +0530

    Update loop.cpp

---
+++ loop.cpp
@@ -0,0 +1,52 @@
+//Use the correct function to read a line of text which is put in by the user.
+
+
+string fullName;
+cout << "Type your full name: ";
+getline
+ (cin, fullName);
+cout << "Your name is: " << fullName;
+
+
+
+//Access the first character (H) in myString and print the result:
+
+
+string myString = "Hello";
+cout << 
+myString[0]
+;
+
+//
+
+Print "Hello World" if x is greater than y.
+
+
+int x = 50;
+int y = 10;
+ (x 
+ y) {
+  cout << "Hello World";
+}
+
+//Print "Hello World" if x is equal to y.
+
+
+int x = 50;
+int y = 50;
+if
+ (x 
+==
+ y) {
+  cout << "Hello World";
+}
+
+
+
+int time = 20;
+if (time < 18) {
+  cout << "Good day.";
+} else {
+  cout << "Good evening.";
+}
+// Outputs "Good evening."
